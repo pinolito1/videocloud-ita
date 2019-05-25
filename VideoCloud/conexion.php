e2130d0abf6fb4e93b6f795fab124dd32cdd8056
pinolito1 <51009509+pinolito1@users.noreply.github.com>
2019-05-25 02:55:45 -0500

    Update conexion.php

--- VideoCloud/conexion.php
+++ VideoCloud/conexion.php
@@ -1,7 +1,7 @@
 <?php  
 $server = 'localhost';
-$username = 'root';
-$password = '';
+$username = 'ari';
+$password = 'pinol';
 $database = 'computo';
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", 
